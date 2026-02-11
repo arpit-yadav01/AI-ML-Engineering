655599a0b395f39a5960541082c1500025296c3d
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-11 16:51:44 +0530

    day 5 completed

---
+++ day05_lists/marks_analyzer.py
@@ -0,0 +1,17 @@
+print("marks analyzer")
+
+marks=[]
+
+for i in range (5):
+    mark= int(input("the marks{i+1}"))
+    marks.append(mark)
+    print("\nAll Marks:", marks)
+    total = sum(marks)
+    average= total/len(marks)
+
+    print("Total Marks:", total)
+    print("Average Marks:", average)
+
+# Find highest & lowest
+print("Highest Mark:", max(marks))
+print("Lowest Mark:", min(marks))
