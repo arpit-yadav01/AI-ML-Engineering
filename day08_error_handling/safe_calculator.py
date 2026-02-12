99c27a66fb365c1457fb5496b43227c4880ea9bf
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-12 15:49:51 +0530

    Learned error handling and built crash-proof calculator

---
+++ day08_error_handling/safe_calculator.py
@@ -0,0 +1,16 @@
+print("lets error handling in the python")
+
+
+nums1= int(input("enter the first number"))
+nums2= int(input("enter the second number"))
+operation = int(input("u can enter the operation { + , - , *, / }"))
+
+
+if(operation == "+"):
+    print("addditions " , nums1+nums2)
+
+elif(operation == "-"):
+    print("sbstraction " ,nums1 - nums2)
+
+elif(operation== "*" ):
+    print("multiplication" , ) 
