d5c2b0868213a0af71301fd359aab38d365ba3ae
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-12 16:05:27 +0530

    Learned error handling and built crash-proof calculator

--- day08_error_handling/safe_calculator.py
+++ day08_error_handling/safe_calculator.py
@@ -1,16 +1,28 @@
 print("lets error handling in the python")
 
 
-nums1= int(input("enter the first number"))
-nums2= int(input("enter the second number"))
-operation = int(input("u can enter the operation { + , - , *, / }"))
+nums1= int(input("enter the first number : "))
+nums2= int(input("enter the second number : "))
+operation = input("u can enter the operation { + , - , *, / }")
 
+try:
+    if(operation == "+"):
+        print("addditions " , nums1+nums2)
 
-if(operation == "+"):
-    print("addditions " , nums1+nums2)
+    elif(operation == "-"):
+     print("sbstraction " ,nums1 - nums2)
 
-elif(operation == "-"):
-    print("sbstraction " ,nums1 - nums2)
+    elif(operation== "*" ):
+     print("multiplication" , nums1*nums2) 
+    elif(operation == "/"):
+        print("divide" , nums1/ nums2)
+    else:
+        print("the put a invalid operation")
 
-elif(operation== "*" ):
-    print("multiplication" , ) 
+except ValueError:
+    print("you put the wrong number")
+except ZeroDivisionError:
+    print("you are dividing number with zero")
+
+except Exception as e:
+    print("the number are invalid")        
