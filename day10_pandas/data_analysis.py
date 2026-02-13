4cb65254be1918b058d83c04310685c6e581126f
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-13 20:41:06 +0530

    pandas

---
+++ day10_pandas/data_analysis.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+print("student analysis")
+
+# Load CSV
+df = pd.read_csv("students.csv")
+
+
+print("\nFull Data:")
+print(df)
+
+print("\nBasic Info:")
+print(df.info())
+
+print("\nAverage Marks:")
+print(df["Marks"].mean())
+
+print("\nHighest Marks:")
+print(df["Marks"].max())
+
+print("\nStudents Above 85 Marks:")
+high_scorers = df[df["Marks"] > 85]
+print(high_scorers)
