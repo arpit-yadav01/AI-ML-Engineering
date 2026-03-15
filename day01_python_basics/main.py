7b8434f0d26e17adb37eb1f3b4a9fb86abdab62a
Arpit Yadav <137641695+arpit-yadav01@users.noreply.github.com>
2026-03-15 20:08:52 +0530

    Update main.py

--- day01_python_basics/main.py
+++ day01_python_basics/main.py
@@ -8,6 +8,7 @@ print("your name is " ,name)
 print("your age is " ,age)
 print("your goal is  ", goal)
 
+
 years = 2
 print("my name is " + name+ " , i m " + str(age) + " old" + "my " + goal )
 print("\nI will master AI in", years, "years! 🔥")
